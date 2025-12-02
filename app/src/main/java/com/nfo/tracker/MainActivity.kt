18de5580a25988f403b7bec0707ad1edeaf8745d
meerabeer <muhammad_abeer@outlook.com>
2025-12-02 11:55:58 +0000

    Wire device health checker into Go On Shift flow

--- app/src/main/java/com/nfo/tracker/MainActivity.kt
+++ app/src/main/java/com/nfo/tracker/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
